53b336db7e7e7218fdc40e99ba9aa0c3c906acee
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2024-02-17 10:44:47 +0200

    Create Source.cpp

---
+++ Exercises from workbook/Chapter 2 - Classes/Exercise 9/Source.cpp
@@ -0,0 +1,10 @@
+#include "Absolute.h"
+#include<iostream>
+
+int main() {
+	Absolute number;
+	std::cout << number.abs(576) << std::endl;
+	std::cout << number.abs(-1.56) << std::endl;
+
+	return 0;
+}
