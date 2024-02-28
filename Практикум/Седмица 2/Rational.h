54af172b20c4b3d7de8ec45e380c1582b8c5611c
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2024-02-28 15:38:02 +0200

    Create Rational.h

---
+++ Практикум/Седмица 2/Rational.h
@@ -0,0 +1,21 @@
+#ifndef _RATIONAL_
+
+class Rational {
+private:
+	int numerator;
+	int denominator;
+
+public:
+	Rational(int _numerator, int _denominator);
+
+	void printRational() const;
+
+	Rational add(const Rational& rhs);
+	Rational subtract(const Rational& rhs);
+	Rational multiply(const Rational& rhs);
+	Rational divide(const Rational& rhs);
+
+	Rational simplify();
+};
+
+#endif // !_RATIONAL_
